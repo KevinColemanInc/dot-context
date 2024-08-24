d0e46ebc006814d485d1fde6df4640f93e48d6e3
heronalps <heronalps@gmail.com>
2024-08-24 14:28:43 -0700

    mz - change .inst to .instruct

--- dto/code_executor.py
+++ dto/code_executor.py
@@ -5,6 +5,6 @@ class CodeExecutor:
     # Persist code from the LLM response and persists to the file path
     def persist_code(llm_response, inst_filepath):
         # Persist the code from the LLM response to the code filepath
-        code_filepath = inst_filepath.removesuffix(".inst")
+        code_filepath = inst_filepath.removesuffix(".instruct")
         with open(code_filepath, "w") as code_file:
             code_file.write(llm_response)
