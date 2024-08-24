4cf47b26123d0c1c9a038373f9758482b786d205
sharonli <2430909+sharonli@users.noreply.github.com>
2024-08-24 14:32:19 -0700

    Add expansion of @import to pull in entire file

--- file_importer.py
+++ file_importer.py
@@ -8,11 +8,9 @@ def import_files(InstFilePath:string) -> ContextManager:
      which contains the .inst and all relevant .context files"""
     contextmanager = ContextManager()
     
-    # put inst filename and contents into contextmanager
+    # put .instruct contents into contextmanager
     abspathinst =  os.path.abspath(InstFilePath)
-    file = open(abspathinst,'r')
-    instfilecontents = file.read()
-    inst = instfilecontents
+    inst = expand_imports(abspathinst)
     contextmanager.set_inst(inst)
     
     dirpath = os.path.dirname(abspathinst)
@@ -21,18 +19,30 @@ def import_files(InstFilePath:string) -> ContextManager:
     for root, dirs, files in os.walk(dirpath, topdown=False):
         for name in files:
             if name.endswith('.context'):
-                # put const filename and contents into contextmanager
+                # put .context contents into contextmanager
                 currentfile = os.path.abspath(name)
-                file = open(currentfile,'r')
-                filecontents = file.read()
-                context = filecontents
+                context = expand_imports(currentfile)
                 contextmanager.add_context(context)
     return contextmanager
 
+def expand_imports(FilePath:string) -> string:
+    expandedfile = ""
+    file = open(FilePath,'r')
+    lines = file.readlines()
+    for line in lines:
+        if line.startswith('@import'):
+            importfilepath = line.split()[1] # get the filepath
+            importfile = open(importfilepath,'r')
+            expandedfile += importfile.read()
+            importfile.close()
+        else:
+            expandedfile += line
+    file.close()
+    return expandedfile
+
 
 # test code
-# contextmanager = import_files("instfile")
+# contextmanager = import_files("instructfile")
 # for context in contextmanager.get_context():
 #     print(context)
-# for instruct in contextmanager.get_inst():
-#     print(instruct)
+# print(contextmanager.get_inst())
