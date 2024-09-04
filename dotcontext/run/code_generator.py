15508fae2a9a995f5512cc7c3ba60f90ec7517a9
Kevin Coleman <kevin.coleman@grabtaxi.com>
2024-09-03 23:14:25 -0400

    add support for langchain

--- dotcontext/run/code_generator.py
+++ dotcontext/run/code_generator.py
@@ -13,11 +13,14 @@ class CodeGenerator:
         elif os.getenv("AZURE_OPENAI_ENDPOINT"):
             from langchain_openai import AzureChatOpenAI
 
-            model = AzureChatOpenAI()
+            model = AzureChatOpenAI(
+                azure_deployment="gpt-35-turbo",
+                api_version="2023-06-01-preview"
+            )
             self.azureChat = model
 
     def generate_code(self):
-        if self.octoAI:
+        if hasattr(self, 'octoAI'):
             response = self.octoAI.text_gen.create_chat_completion(
                 max_tokens=65536,
                 messages=self.messages,
@@ -31,10 +34,13 @@ class CodeGenerator:
         else:
             from langchain_core.messages import HumanMessage, SystemMessage
 
-            messages = [
-                HumanMessage(content=messages[0].content),
-                SystemMessage(content=messages[1].content),
+            langChainMessages = [
+                HumanMessage(content=self.messages[0].content),
+                SystemMessage(content=self.messages[1].content),
             ]
-            response = self.azureChat.invoke(messages)
+            response = self.azureChat.invoke(langChainMessages)
+            print("\n\n" + langChainMessages[0].content + "\n---\n")
+            print("\n\n" + langChainMessages[1].content + "\n---\n")
+            print("\n\n" + response.content+ "\n")
 
             return response.content
